b453c36aa244f2b0951d13fab595f931f02c2935
Wojciech Jarosz <jaroszw@gmail.com>
2021-02-01 13:27:42 +0100

    before deploy

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-let topHeader = document.getElementById('header-container');
-const line = document.querySelector('.menu-icon__line');
-const title = document.getElementById('popular');
-const list = document.querySelector('.navbar__list');
-const links = list.querySelectorAll('li a');
-const menuBtn = document.querySelector('.menu-btn');
-const menuIcon = document.querySelector('.menu-icon');
+let topHeader = document.getElementById("header-container");
+const line = document.querySelector(".menu-icon__line");
+const title = document.getElementById("popular");
+const list = document.querySelector(".navbar__list");
+const links = list.querySelectorAll("li a");
+const menuBtn = document.querySelector(".menu-btn");
+const menuIcon = document.querySelector(".menu-icon");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > 10) {
-    topHeader.classList.add('scrolled');
-    line.classList.add('move');
+    topHeader.classList.add("scrolled");
+    line.classList.add("move");
   } else {
-    topHeader.classList.remove('scrolled');
-    line.classList.remove('move');
+    topHeader.classList.remove("scrolled");
+    line.classList.remove("move");
   }
 });
 
-menuIcon.addEventListener('click', () => {
-  menuIcon.classList.toggle('open');
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("open");
 });
 
 links.forEach((link) =>
-  link.addEventListener('click', function (e) {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
-    const elementID = this.getAttribute('id');
+    const elementID = this.getAttribute("id");
     const selectedSection = document.getElementById(`${elementID}-sections`);
     window.scrollTo({
       top: `${selectedSection.offsetTop - topHeader.offsetHeight - 100}`,
       left: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
-    menuIcon.classList.remove('open');
+    menuIcon.classList.remove("open");
   })
 );
 
@@ -40,10 +40,10 @@ $(function () {
       singleDatePicker: true,
       showDropdowns: true,
       minYear: 2000,
-      maxYear: parseInt(moment().format('YYYY'), 10),
+      maxYear: parseInt(moment().format("YYYY"), 10),
     },
     function (start, end, label) {
-      var years = moment().diff(start, 'years');
+      var years = moment().diff(start, "years");
     }
   );
 });
